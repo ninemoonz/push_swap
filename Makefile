5b71d6406ecd4aab8175bea78b8c9dcfc45f5c85
Koodal Kweon <kkweon@f1r4s20.codam.nl>
2026-02-12 16:17:34 +0100

    add libft and ft_printf

--- Makefile
+++ Makefile
@@ -1,13 +1,24 @@
-NAME = push_swap.a
+NAME = push_swap
+
+# Directories
+LIBFT_DIR = libft
+FT_PRINTF_DIR = ft_printf
+SRC_DIR = src
+INC_DIR = include
+
+# Libraries
+LIBFT = $(LIBFT_DIR)/libft.a
+FT_PRINTF = $(FT_PRINTF_DIR)/libftprintf.a
 
 CC := cc
 CFLAGS := -Wall -Wextra -Werror -I.
+INCLUDE = -I$(INC_DIR) -I$(LIBFT_DIR) -I$(FT_PRINTF_DIR)
 
-SRC =
-
+SRC = $(SRC_DIR)/main.c \
+		
 OBJ = $(SRC:%.c=%.o)
 
-all: $(NAME)
+all: $(NAME) $(FT_PRINTF) $(LIBFT)
 
 $(NAME): $(OBJ)
 	ar rcs $(CFLAGS) -c $< -o $@
--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,26 @@
 /*   By: kkweon <kkweon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 15:44:01 by kkweon            #+#    #+#             */
-/*   Updated: 2026/02/09 15:44:11 by kkweon           ###   ########.fr       */
+/*   Updated: 2026/02/12 15:44:46 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main (void)
+#include "push_swap.h"
+
+int main (int argc, char **argv)
 {
-    return (0);
+	char **input;
+
+	if (argc < 2) // no arguments
+		return (1);
+	else if (argc == 2) // when the arguments are passed in as a string ./out "1 2 3 4 5"
+	{
+		input = ft_split(argv[1], ' ');
+
+	}
+	else // when the arguments are passed in as multiple arguments ./out 1 2 3 4 5
+	{
+
+	}
+	return (0);
 }
