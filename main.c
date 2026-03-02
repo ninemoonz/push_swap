7b42c51bf1ee96a748a1a1e2abda9fddc7164dff
Koodal Kweon <kkweon@f1r4s6.codam.nl>
2026-03-02 18:29:24 +0100

    until reverse rotate

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kkweon <kkweon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 17:14:24 by kkweon            #+#    #+#             */
-/*   Updated: 2026/03/02 15:03:44 by kkweon           ###   ########.fr       */
+/*   Updated: 2026/03/02 15:22:50 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,27 @@ t_node	*build_stack(int *num_arr, int len)
 int	main(int argc, char **argv)
 {
 	t_node	*stack_a;
+	char	**tmp;
 	int		*num_arr;
 	int		len;
 
 	stack_a = NULL;
 	if (argc == 2)
-		len = count_strarr(ft_split(argv[1], ' '));
+	{
+		tmp = ft_split(argv[1], ' ');
+		len = count_strarr(tmp);
+		split_free(tmp, len);
+	}
 	else
 		len = argc - 1;
 	num_arr = num_check(argc, argv);
 	stack_a = build_stack(num_arr, len);
+	while (stack_a)
+	{
+		printf("stack value: %d\n", stack_a->value);
+		stack_a = stack_a->next;
+	}
+	free(num_arr);
+	free_stack(stack_a);
 	return (0);
 }
--- move.c
+++ move.c
@@ -6,8 +6,145 @@
 /*   By: kkweon <kkweon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 15:27:00 by kkweon            #+#    #+#             */
-/*   Updated: 2026/02/26 15:51:51 by kkweon           ###   ########.fr       */
+/*   Updated: 2026/03/02 18:28:44 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	remove_top(t_node **stack)
+{
+	t_node	*tmp;
+	int		value;
+
+	tmp = *stack;
+	value = tmp->value;
+	*stack = tmp->next;
+	free(tmp);
+	return (value);
+}
+
+void	swap(t_node **stack)
+{
+	int	tmp;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	tmp = (*stack)->value;
+	(*stack)->value = (*stack)->next->value;
+	(*stack)->next->value = tmp;
+}
+
+void	sa(t_node **stack_a)
+{
+	swap(stack_a);
+	ft_printf("sa\n");
+}
+
+void	sb(t_node **stack_b)
+{
+	swap(stack_b);
+	ft_printf("sb\n");
+}
+
+void	ss(t_node **stack_a, t_node **stack_b)
+{
+	swap(stack_a);
+	swap(stack_b);
+	ft_printf("ss\n");
+}
+
+void	push(t_node **stack, int value)
+{
+	t_node	*new_top;
+
+	new_top = create_node(value);
+	if (!new_top)
+		return ;
+	new_top->next = *stack;
+	*stack = new_top;
+}
+
+void	pa(t_node **stack_a, t_node **stack_b)
+{
+	if (!*stack_b)
+		return ;
+	push(stack_a, remove_top(stack_b));
+	ft_printf("pa\n");
+}
+
+void	pb(t_node **stack_a, t_node **stack_b)
+{
+	if (!*stack_a)
+		return ;
+	push(stack_b, remove_top(stack_a));
+	ft_printf("pb\n");
+}
+
+void	rotate(t_node **stack)
+{
+	t_node	*tmp;
+	t_node	*last_node;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	last_node = *stack;
+	*stack = last_node->next;
+	tmp = *stack;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = last_node;
+	last_node->next = NULL;
+}
+
+void	ra(t_node **stack_a)
+{
+	rotate(stack_a);
+	ft_printf("ra\n");
+}
+
+void	rb(t_node **stack_b)
+{
+	rotate(stack_b);
+	ft_printf("rb\n");
+}
+
+void	rr(t_node **stack_a, t_node **stack_b)
+{
+	rotate(stack_a);
+	rotate(stack_b);
+	ft_printf("rr\n");
+}
+
+void	reverse_rotate(t_node **stack)
+{
+	t_node	*tmp;
+	t_node	*first_node;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	tmp = *stack;
+	while (tmp->next->next)
+		tmp = tmp->next;
+	first_node = tmp;
+	*stack = first_node;
+}
+
+void	rra(t_node **stack_a)
+{
+	reverse_rotate(stack_a);
+	ft_printf("rra\n");
+}
+
+void	rrb(t_node **stack_b)
+{
+	reverse_rotate(stack_b);
+	ft_printf("rrb\n");
+}
+
+void	rrr(t_node **stack_a, t_node **stack_b)
+{
+	reverse_rotate(stack_a);
+	reverse_rotate(stack_b);
+	ft_printf("rrr\n");
+}
